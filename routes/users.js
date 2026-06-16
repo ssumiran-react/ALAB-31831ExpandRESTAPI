b1b38df08367a0405a7bef0424847261bf8a6424
ssumiran <ssumiran@yahoo.com>
2026-06-16 14:56:11 -0400

    Initial and part 1 in progress

---
+++ routes/users.js
@@ -0,0 +1,94 @@
+import express from 'express'
+const router = express.Router()
+
+import users from '../data/users.js'
+import posts from '../data/posts.js'
+import error from '../utilities/error.js'
+
+router
+  .route("/")
+  .get((req, res) => {
+    const links = [
+      {
+        href: "users/:id",
+        rel: ":id",
+        type: "GET",
+      },
+    ];
+
+    res.json({ users, links });
+  })
+  .post((req, res, next) => {
+    if (req.body.name && req.body.username && req.body.email) {
+      if (users.find((u) => u.username == req.body.username)) {
+        next(error(409, "Username Already Taken"));
+      }
+
+      const user = {
+        id: users[users.length - 1].id + 1,
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+      };
+
+      users.push(user);
+      res.json(users[users.length - 1]);
+    } else next(error(400, "Insufficient Data"));
+  });
+
+router
+  .route("/:id")
+  .get((req, res, next) => {
+    const user = users.find((u) => u.id == req.params.id);
+
+    const links = [
+      {
+        href: `/${req.params.id}`,
+        rel: "",
+        type: "PATCH",
+      },
+      {
+        href: `/${req.params.id}`,
+        rel: "",
+        type: "DELETE",
+      },
+    ];
+
+    if (user) res.json({ user, links });
+    else next();
+  })
+  .patch((req, res, next) => {
+    const user = users.find((u, i) => {
+      if (u.id == req.params.id) {
+        for (const key in req.body) {
+          users[i][key] = req.body[key];
+        }
+        return true;
+      }
+    });
+
+    if (user) res.json(user);
+    else next();
+  })
+  .delete((req, res, next) => {
+    const user = users.find((u, i) => {
+      if (u.id == req.params.id) {
+        users.splice(i, 1);
+        return true;
+      }
+    });
+
+    if (user) res.json(user);
+    else next();
+  });
+
+router
+  .route("/:id/posts")
+  .get((req, res, next) => {
+    const user = users.find((u) => u.id == req.params.id);
+    let post = posts.filter((p) => p.userId == user.id);
+
+    res.json({ post });
+  }
+) 
+export default router
